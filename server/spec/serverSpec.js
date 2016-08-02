6baf05062025753482c32afd88d110db0c2831ef
rana1926 <rana.kelani@gmail.com>
2016-08-02 15:32:51 +0300

    Fix minor bugs in server. closes #30

--- server/spec/serverSpec.js
+++ server/spec/serverSpec.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 
 var server = require(path.join(__dirname, '..', './server.js'));
 var userController = require(path.join(__dirname, '..', '../server/users/userController.js'));
-
+var gameController = require(path.join(__dirname,'..','../server/games/gameController.js'))
 
 
 /*                       USER CONTROLLER                              */
@@ -17,9 +17,39 @@ describe('userController', function () {
 
   it('have function signup', function () {
     expect(userController.signup).to.be.a('function');
+  });  
+});
+
+//=====================================================================
+/*                       GAME CONTROLLER                              */
+//=====================================================================
+describe('gameController', function () {
+  'use strict';
+  it('have function getAllGames', function () {
+    expect(gameController.getAllGames).to.be.a('function');
   });
 
+  it('have function getGame', function () {
+    expect(gameController.getGame).to.be.a('function');
+  });
 
-  
+  it('have function createGame', function () {
+    expect(gameController.createGame).to.be.a('function');
+  });
+
+  it('have function insertPlayer', function () {
+    expect(gameController.insertPlayer).to.be.a('function');
+  });
+
+  it('have function editGame', function () {
+    expect(gameController.editGame).to.be.a('function');
+  });
+
+  it('have function removePlayer', function () {
+    expect(gameController.removePlayer).to.be.a('function');
+  });  
 });
+
+
+
 //=====================================================================
