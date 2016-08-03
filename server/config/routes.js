1d4b082bb4ef2530da736020ddff381c0be7e4c0
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-08-03 12:18:06 +0300

    Add user routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,6 +6,7 @@ var helpers = require('./helpers.js');
 module.exports = function(app, express) {
 	app.post('api/users/signup', user.signup);
 	app.post('api/users/singin', user.signin);
+	
 	/*app.get('api/user/:id', user.getUser);
 	app.post('api/users', user.getPlayers);
 	app.put('api/user/:id', user.editUser );
