ed44b7380da4c7c881c5887195d63fefc5d1c17d
Saif Elokour <saiffelokour@gmail.com>
2016-08-03 12:28:56 +0300

    incomplete basic test for getUser

--- server/spec/serverSpec.js
+++ server/spec/serverSpec.js
@@ -1,16 +1,33 @@
 var path = require('path');
 var expect = require('chai').expect;
+var mongoose = require('mongoose');
 
 var server = require(path.join(__dirname, '..', './server.js'));
 
 var userController = require(path.join(__dirname, '..', '../server/users/userController.js'));
+var User = require(path.join(__dirname, '..', '../server/users/userModel.js'));
 var gameController = require(path.join(__dirname,'..','../server/games/gameController.js'))
 
+
+var dbURI = 'mongodb://localhost/teamupTEST';
+
+// The `clearDB` helper function, when invoked, will clear the database
+var clearDB = function (done) {
+  //mongoose.connection.collections['User'].remove(done);
+  done()
+};
 
 /*                       USER CONTROLLER                              */
 //=====================================================================
 describe('userController', function () {
   'use strict';
+
+  before(function(done){
+  	if(mongoose.connection.db) {
+      return done();
+    }
+    mongoose.connect(dbURI, done);
+  });
 
   it('have function signin', function () {
     expect(userController.signin).to.be.a('function');
@@ -30,6 +47,29 @@ describe('userController', function () {
 
   it('have function getPlayers', function () {
     expect(userController.getPlayers).to.be.a('function');
+  });
+
+  describe('userController', function () {
+	beforeEach(function(done){
+		clearDB(function(){
+			var users = [{
+				username: "fakeUser",
+				firstName: "fake",
+				lastName: "McFaker",
+				password: "password",
+				email: "fake@faker.com",
+			}];
+
+			User.create(users, done)
+		});
+	});
+
+  it('getUser should return a User by ID', function () {
+		userController.getUser({_id: "57a1b478b0307ef0429dd9ff"}, function(err, user){
+			expect(user.username).to.equal('fakeUser');
+			done()
+		})
+	});
   });
 
 });
