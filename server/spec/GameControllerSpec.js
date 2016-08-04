0ce48267f0976c8a0a5123e350a816687328fbd6
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-04 17:18:51 +0300

    (test) Add test for route /api/games

--- server/spec/GameControllerSpec.js
+++ server/spec/GameControllerSpec.js
@@ -1,7 +1,9 @@
-var expect = require('chai').expect;
-
-var server = require('../server.js');
-var gameController = require('../games/gameController.js');
+var chai = require('chai');
+var expect = chai.expect;
+var request = require('supertest');
+var app = require('../server.js');
+var GAME = require('../games/gameModel.js');
+var gameController=require('../games/gameController.js')
 
 //=====================================================================
 /*                       GAME CONTROLLER                              */
@@ -11,6 +13,19 @@ describe('gameController', function () {
   it('have function getAllGames', function () {
     expect(gameController.getAllGames).to.be.a('function');
   });
+
+  it('responds with a 200 (OK)', function (done) {
+
+        request(app)
+          .get('/api/games')
+          .expect(200)
+          .end(function(err,res){
+            if(err) return done(err);
+            console.log(res.body);
+            done();
+          })
+
+      });
 
   it('have function getGame', function () {
     expect(gameController.getGame).to.be.a('function');
