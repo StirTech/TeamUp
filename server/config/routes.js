40b6966ba37db82d9c28198872909d4bcc795549
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-04 17:18:03 +0300

    (test) Add test for route /api/games

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,20 +4,23 @@ var game = require('../games/gameController.js');
 var helpers = require('./helpers.js');
 
 module.exports = function(app, express) {
-	app.post('/api/users/signup', user.signup);
-	app.get('/api/users/singin', user.signin);
+
+	// app.post('api/users/signup', user.signup);
+	// app.post('api/users/singin', user.signin);
 	
 	/*app.get('api/user/:id', user.getUser);
 	app.post('api/users', user.getPlayers);
 	app.put('api/user/:id', user.editUser );
 
-	app.get('api/games', game.getAllGames);
 	app.get('api/game/:id', game.getGame);
 	app.post('api/game', game.createGame);
 	app.post('api/game', game.insertPlayer);
 	app.put('api/game/:id', game.editGame);
 	app.delete('api/game/:id', game.removePlayer);*/
 	
+	app.get('/api/games', game.getAllGames);
+
+
 	app.use(helpers.errorLogger);
 	app.use(helpers.errorHandler);
 };
