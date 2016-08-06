e0aacfa42f301231ba1171d1d6ebfa63c3fa3355
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-06 14:58:45 +0300

    (feat) Implement services file closes #81

---
+++ client/app/services/services.js
@@ -0,0 +1,13 @@
+angular.module('TeamUp.services', [])
+
+.factory('Users', function ($http) {
+
+})
+
+.factory('Games', function ($http) {
+
+})
+
+.factory('Auth', function ($http, $location, $window) {
+
+})
