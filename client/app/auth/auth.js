53a65c6a828ea7e4a1d75fd953c7f8689a97ef71
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-08-07 16:32:55 +0300

    (feat) Implement authController

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -61,5 +61,25 @@ angular.module('TeamUp.auth', [])
       $scope.signinUserData.password = password;
       UserAuth.signUser($scope.signinUserData)
     }
+  $scope.userData={
+	username:'',
+	firstName:'',
+	lastName:'',
+	password:'',
+	email:''
+  }
+
+
+
+  $scope.createUser=function () {
+    UserAuth.addNewUser(userData)
+  }
+
+  $scope.signin=function (username,password) {
+    var user.username = username;
+    user.password = password;
+    
+    UserAuth.signUser(user)
+  }
   
 })
