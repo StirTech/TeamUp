41cbb05dad65bbdfc67fc485667ffd7e198a5015
Saif Elokour <saiffelokour@gmail.com>
2016-08-08 14:57:39 +0300

    (fix) merge conflicts

--- client/app/app.js
+++ client/app/app.js
@@ -52,22 +52,3 @@ angular.module('TeamUp', [
     // of interceptors. Think of it like middleware for your ajax calls
     //$httpProvider.interceptors.push('AttachTokens');
 });
-
-angular.module('TeamUp'[
-	'TeamUp.Auth',
-	'TeamUp.services',
-	'ngRoute'
-	])
-.config(function ($routeProvider, $httpProvider) {
-	$routeProvider
-	.when('/signin',{
-		templateUrl : 'app/auth/signin.html'
-		controller  : 'app/auth/auth.js',
-	})
-	.when('/signup',{
-		templateUrl : 'app/auth/signup.html'
-		controller  : 'app/auth/auth.js',
-	})
-
-	 $httpProvider.interceptors.push('AttachTokens');
-})
