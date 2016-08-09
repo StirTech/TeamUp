478c54d4907fe9f319f8319ec904644c1e65db18
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-09 15:50:23 +0300

    (fix) Fix the implement of signin and signup

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,49 +1,65 @@
 angular.module('TeamUp.auth', [])
 
-  .controller('AuthController', function ($scope, $window, $location, UserAuth ) {
-    $scope.createUserData={};
-    $scope.signinUserData={};
-    $window.islogin = false;
+  .controller('AuthController', function ($scope, $window, $location, UserAuth, $route ) {
+    $scope.wrong=true;
 
-    $scope.setcreateUserData = function (firstName, lastName, username, password, email) {
-      $scope.createUserData.username = username;
-      $scope.createUserData.password = password;
-      $scope.createUserData.email = email;
-      $scope.createUserData.firstName = firstName;
-      $scope.createUserData.lastName = lastName;
-      UserAuth.addNewUser($scope.createUserData)
-    }
-    
+    $scope.signin = function () {
+      UserAuth.signUser($scope.user)
+      .then(function (data) {
+        $scope.wrong=false;
+        $window.localStorage['token'] = data.token;
+        $window.localStorage['userId'] = data.userId;
+        $window.localStorage['isLogin'] = true;
+        $location.path('/home');
+        $window.location.reload();
+      })
+      .catch(function (err) {
+        console.log(err);
+        $scope.wrong=false;
+        $scope.username="";
+        $scope.password="";
+      })
+    };
 
-    //===============================================================================================
-    /*                                        login/out Auth                                       */
-    //===============================================================================================
-    
-    $scope.islogin = function () {
-      if($window.localStorage['token']){
-        $window.islogin = true;
-      }else{
-        $window.islogin = false;
-      }
-      console.log($window.islogin)
+    $scope.signup = function () {
+        UserAuth.addNewUser($scope.user)
+        .then(function (user) {
+            $scope.signin();
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
     }
+
+    // //===============================================================================================
+    // /*                                        login/out Auth                                       */
+    // //===============================================================================================
     
-    //===============================================================================================
-    /*                                             signin                                          */
-    //===============================================================================================
+    // $scope.islogin = function () {
+    //   if($window.localStorage['token']){
+    //     $window.islogin = true;
+    //   }else{
+    //     $window.islogin = false;
+    //   }
+    //   console.log($window.islogin)
+    // }
     
-    $scope.signin=function (username,password) {
-      $scope.signinUserData.username = username;
-      $scope.signinUserData.password = password;
-      UserAuth.signUser($scope.signinUserData)
-      .then(function (resp) {
-        console.log(resp)
-        $window.localStorage['token'] = resp.token;
-        $window.localStorage['userId'] = resp.userId;
-      })
-      .then(function () {
-        $scope.islogin();
-      })
-    }
+    // //===============================================================================================
+    //                                              signin                                          
+    // //===============================================================================================
+    
+    // $scope.signin=function (username,password) {
+    //   $scope.signinUserData.username = username;
+    //   $scope.signinUserData.password = password;
+    //   UserAuth.signUser($scope.signinUserData)
+    //   .then(function (data) {
+    //     console.log(resp)
+    //     $window.localStorage['token'] = resp.token;
+    //     $window.localStorage['userId'] = resp.userId;
+    //   })
+    //   .then(function () {
+    //     $scope.islogin();
+    //   })
+    // }
     
 })
