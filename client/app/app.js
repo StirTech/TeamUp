543fcc959653b17117cc8bdc9360cd7a90586920
rana1926 <rana.kelani@gmail.com>
2016-08-09 20:33:43 +0300

    (feat)Implement google maps for createGame

--- client/app/app.js
+++ client/app/app.js
@@ -7,8 +7,8 @@ angular.module('TeamUp', [
   'TeamUp.editGame',
   'TeamUp.profile',
   'TeamUp.editProfile',
-  'ngRoute'
-
+  'ngRoute',
+  'ngMap'
 ])
 
 
@@ -28,7 +28,16 @@ angular.module('TeamUp', [
     $location.path('/home')
   }
 })
-
+.controller('EventArgumentsController', function(NgMap) {
+    var vm = this;
+    NgMap.getMap().then(function(map) {
+      vm.map = map;
+    });
+    vm.placeMarker = function(e) {
+      var marker = new google.maps.Marker({position: e.latLng, map: vm.map});
+      vm.map.panTo(e.latLng);
+    }
+  })
 .config(function ($routeProvider, $httpProvider) {
   $routeProvider
     .when('/home', {
