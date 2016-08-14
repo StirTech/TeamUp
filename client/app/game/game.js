264ed67475eace1dace0823337267e68e8ba7249
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-14 17:16:51 +0300

    (style) Add new style for game page

--- client/app/game/game.js
+++ client/app/game/game.js
@@ -1,19 +1,31 @@
 angular.module('TeamUp.game',[])
 
-.controller('gameController',function($scope, Game, User, facebook, $routeParams, $location){
+.controller('gameController',function($scope, Game, User, facebook, $routeParams, $location, $window){
 	$scope.game={};
-	$scope.game.owner ="";
+	$scope.owner ={};
 	$scope.game.plyersObjs =[];
+	$scope.numberOfPlayer;
+	$scope.joinButton="join";
 
 	$scope.initlize = function () {
 		Game.getOne($routeParams.id)
 		.then(function (game) {
 			$scope.game=game;	
-			//$scope.game.plyersObjs=
-			User.getPlayers(game.players)
-			.then(function (players) {
-				console.log(players);
-				$scope.game.plyersObjs=players;
+			User.getUser(game.owner)
+			.then(function (user) {
+				$scope.owner=user
+			})
+			.catch(function (err) {
+				console.log(err)
+			})
+			//$scope.game.plyersOb	js=
+			if($scope.game.players.indexOf($window.localStorage.userId)!==-1)
+				$scope.joinButton="leave";
+			User.getPlayers({playerIds :$scope.game.players})
+			.then(function (data) {
+				$scope.game.plyersObjs=data.data;
+				$scope.numberOfPlayer=$scope.game.plyersObjs.length;
+				console.log("numberOfPlayer : ",$scope.numberOfPlayer);
 			})
 			.catch(function (err) {
 				console.log(err);
@@ -26,6 +38,8 @@ angular.module('TeamUp.game',[])
 	
 	$scope.initlize();
 	$scope.fbshare = function(){
+		console.log('so3ad');
 		facebook.share($routeParams.id, $scope.game.picture, $scope.game.name);	
-	}
+	};
+	
 });
