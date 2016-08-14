533afe679de884dce322c47962ba1f13d5246741
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-14 19:29:29 +0300

    (fix) Fix the Jion function

--- client/app/game/game.js
+++ client/app/game/game.js
@@ -6,8 +6,11 @@ angular.module('TeamUp.game',[])
 	$scope.game.plyersObjs =[];
 	$scope.numberOfPlayer;
 	$scope.joinButton="join";
+	$scope.playerId = $window.localStorage.userId;
+	console.log($scope.playerId);
 
 	$scope.initlize = function () {
+		console.log($scope.game);
 		Game.getOne($routeParams.id)
 		.then(function (game) {
 			$scope.game=game;	
@@ -41,12 +44,35 @@ angular.module('TeamUp.game',[])
 		console.log('so3ad');
 		facebook.share($routeParams.id, $scope.game.picture, $scope.game.name);	
 	};
+	
+	// $scope.joinGame = function(){
+	// 	Game.insertPlayer($scope.game._id,$window.localStorage.userId)
+	// 	.then(function (game) {
+	// 		$scope.initlize()
+	// 	})
+	// 	.catch(function (err) {
+	// 		console.log(err)
+	// 	})
+	// };
+
+	// $scope.leaveGame = function(){
+	// 	Game.removePlayer($scope.game._id,$window.localStorage.userId)
+	// 	.then(function(game) {
+	// 		$scope.initlize();
+	// 	})
+	// 	.catch(function (err) {
+	// 		console.log(err)
+	// 	})
+	// };
+
 	$scope.joinGame =function () {
 		if($scope.game.players.indexOf($window.localStorage.userId)===-1){
+			console.log("Join");
 			$scope.joinButton="Leave"
 			Game.insertPlayer($scope.game._id,$window.localStorage.userId)
 			.then(function (game) {
 				$scope.game=game;
+				// console.log(game);
 				$scope.initlize()
 			})
 			.catch(function (err) {
@@ -54,24 +80,19 @@ angular.module('TeamUp.game',[])
 			})
 		}
 		else {
-			$scope.joinButton="Join"
-
-			for (var i = 0; i < $scope.game.players.length; i++) {
-				if($scope.game.players[i]===$window.localStorage.userId){
-					$scope.game.players.splice(i,1);
-				}
-			}
-
-			User.getPlayers({playerIds :$scope.game.players})
-			.then(function (data) {
-				$scope.game.plyersObjs=data.data;
-				$scope.numberOfPlayer=$scope.game.plyersObjs.length;
-				console.log("numberOfPlayer : ",$scope.numberOfPlayer);
-				//Game.removePlayers($scope.game._id,$window.localStorage.userId);
+			console.log("Leave");
+			$scope.joinButton="Join";
+			Game.removePlayer($scope.game._id,$window.localStorage.userId)
+			.then(function(game) {
+				console.log(game);
+				// $scope.game=game;
+				$scope.initlize();
 			})
 			.catch(function (err) {
-				console.log(err);
+				console.log(err)
 			})
+
+
 		}
 	}
 });
