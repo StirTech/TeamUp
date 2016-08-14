39a616924c568cb708489e2f5350ae6e3474ff1b
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-08-14 14:45:33 +0300

    (style) button style

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -26,7 +26,7 @@ angular.module('TeamUp.profile',[])
 				 ['Last Name','lastName',$scope.user.lastName],
 				 ['Email','email',$scope.user.email],
 				 ['Country','country',$scope.user.country],
-				 ['City','city',$scope.user.city.replace('`', '').replace(",","")]
+				 ['City','city',$scope.user.city]
 			]
 			for (var i = 0; i < $scope.MissingInfo.length; i++) {
 				if($scope.MissingInfo[i][2].length !== 0){
