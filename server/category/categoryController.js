7362aa5a5dd3957008116d48b259f6035c8206a5
Saif Elokour <saiffelokour@gmail.com>
2016-08-15 19:36:08 +0300

    (feat) adds getAll function to categoryController

--- server/category/categoryController.js
+++ server/category/categoryController.js
@@ -1,5 +1,30 @@
-var Game = require('./gameModel.js');
+var Game = require('../game/gameModel.js');
+var Category = require('./categoryModel.js');
 
 module.exports = {
-	
+
+	getAll: function(req, res, next){
+		Category.find().exec(function (err,allCategories) {
+			if(err)
+				res.status(500).send('err');
+			else
+				res.status(200).send(allCategories);
+		});
+	},
+
+	getOne: function(req, res, next){
+
+	},
+
+	addOne: function(req, res, next){
+
+	},
+
+	editOne: function(req, res, next){
+
+	},
+
+	deleteOne: function(req, res, next){
+
+	}
 }
