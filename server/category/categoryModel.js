9e9913cac0aa5d1ba5a703aad2cedaa34c0dfaa9
Saif Elokour <saiffelokour@gmail.com>
2016-08-15 19:37:07 +0300

    (feat) adds name, games and picture to categoryModel schema

--- server/category/categoryModel.js
+++ server/category/categoryModel.js
@@ -1,7 +1,18 @@
 var mongoose = require ('mongoose');
 
-var CategorySchema = = new mongoose.Schema({
-
+var CategorySchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	}, 
+	games: [{
+		type: String,
+		required: true
+	}], 
+	picture: {
+		type: String,
+		required: true
+	}
 })
 
 var Category = mongoose.model('Category', CategorySchema);
