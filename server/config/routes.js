2383a2d504bdfa48c5853e6b57605e5961af988d
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-08-17 16:08:11 +0300

    add like & unlike route

--- server/config/routes.js
+++ server/config/routes.js
@@ -25,13 +25,13 @@ module.exports = function(app, express) {
 	app.put('/api/game/removePlayer/:id', game.removePlayer);
 	app.put('/api/game/:id/edit', game.editGame);
 	app.post('/api/game/:id', game.insertPlayer);
-	
+	app.post('/api/game/:id/like',game.likeGame);
+	app.post('/api/game/:id/unlikeGame',game.unlikeGame);
 
 	//Comment Route
 	app.post('/api/comment',comment.insertComment);
 	app.get('/api/comments/:id',comment.getComments)
 	
-
 
 	// category routes
 	app.get('/api/categories', category.getAll);
