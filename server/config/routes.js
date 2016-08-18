44455b984d6943e2b3ded155385a6ba81b2752ef
rana1926 <rana.kelani@gmail.com>
2016-08-18 17:54:21 +0300

    (feat)Add file upload using multer

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,22 +3,9 @@ var game = require('../games/gameController.js');
 var comment = require ('../comment/commentController.js');
 var category = require('../category/categoryController.js');
 var rate = require('../rate/rateController.js');
+var utils = require('./utils.js')
 
 var helpers = require('./helpers.js');
- var multer = require('multer');
- var storage = multer.diskStorage({ //multers disk storage settings
-        destination: function (req, file, cb) {
-            cb(null, './uploads/');
-        },
-        filename: function (req, file, cb) {
-            var datetimestamp = Date.now();
-            cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
-        }
-    });
-
-    var upload = multer({ //multer settings
-                    storage: storage
-                }).single('file');
 
 
 module.exports = function(app, express) {
@@ -60,15 +47,8 @@ module.exports = function(app, express) {
 	app.put('/api/category/:id/edit', category.editCategory);
 	app.delete('/api/category/:id/delete', category.deleteCategory);
 
-	app.post('/api/upload', function(req, res) {
-        upload(req,res,function(err){
-            if(err){
-                 res.json({error_code:1,err_desc:err});
-                 return;
-            }
-             res.json({error_code:0,err_desc:null});
-        });
-    });
+	//upload an image
+	app.post('/api/upload', utils.uploadImg);
 
 	//error handling
 	app.use(helpers.errorLogger);
