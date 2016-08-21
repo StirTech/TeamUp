cad92f77c9022e985567039b04a5b1b4e0e92fc1
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-21 15:31:14 +0300

    (fix) Add ssl to pubnub

--- client/js/InitializePubNub.js
+++ client/js/InitializePubNub.js
@@ -1,6 +1,7 @@
 var pubnub = PUBNUB.init({
     publish_key: 'pub-c-dd62d128-affa-442e-9468-962e09c7ec7a',
     subscribe_key: 'sub-c-46d9ccf4-66b1-11e6-9652-0619f8945a4f',
+    ssl:true,
     error: function (error) {
         console.log('Error:', error);
     }
