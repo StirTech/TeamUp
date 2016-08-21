624630c7f5fd6192d635b8a10deb8f2db252c0d8
Saif Elokour <saiffelokour@gmail.com>
2016-08-21 16:28:20 +0300

    (feat) adds initialize function to categoryController

--- client/app/category/category.js
+++ client/app/category/category.js
@@ -2,5 +2,18 @@ angular.module('TeamUp.category',[])
 
 .controller('categoryController', function($scope, Category, Game, $location, $window, $routeParams, $interval){
 
+	$scope.categories = []
+
+	$scope.initialize = function(){
+		Category.getAll()
+		.then(function(categories){
+			$scope.categories = categories
+		})
+		.catch(function(err){
+			console.log(err)
+		})
+	}
+
+	$scope.initialize()
 })
 	
