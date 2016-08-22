e3755ab3b9a95a74a2a209c62127056a194fa8ab
Saif Elokour <saiffelokour@gmail.com>
2016-08-22 14:02:20 +0300

    (feat) adds function getGames in categoryController

--- client/app/category/category.js
+++ client/app/category/category.js
@@ -3,14 +3,31 @@ angular.module('TeamUp.category',[])
 .controller('categoryController', function($scope, Category, Game, $location, $window, $routeParams, $interval){
 
 	$scope.categories = []
+	$scope.games = []
+	$scope.type = {value: '', filter: false}
+	$scope.cat = {value: '', filter: false}
 
 	$scope.initialize = function(){
 		Category.getAll()
 		.then(function(categories){
 			$scope.categories = categories
 		})
+		.then(function(){
+			$scope.getGames()
+		})
 		.catch(function(err){
 			console.log(err)
+		})
+	}
+
+
+	$scope.getGames = function(){
+		Game.getAll()
+		.then(function(games){
+			$scope.games = games
+		})
+		.catch(function(err){
+			throw err
 		})
 	}
 
