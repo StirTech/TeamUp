c12269ba633d1a7f37bc3e7c178ec7d212810134
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-08-22 10:10:30 +0300

    (refactor) remove removePin

--- client/app/game/createGame.js
+++ client/app/game/createGame.js
@@ -63,11 +63,6 @@ angular.module('TeamUp.createGame',[])
       $scope.map = map;
     });
 
-	$scope.removePin = function () {
-		if ($window.marker) {
-    		$window.marker.setMap(null);
-    	}
-	}
 	
     $scope.placeMarker = function(e) {// place a red marker on the map and get the game location from the marker position
     	if ($window.marker) {
